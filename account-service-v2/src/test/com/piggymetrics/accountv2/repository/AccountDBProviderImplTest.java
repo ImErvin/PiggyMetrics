95fe8e149529b4d52239d89bec70d929b1974db1
imervin <employervin@gmail.com>
2018-11-20 13:17:01 +0000

    Added verification tests for Account object and Item object

--- account-service-v2/src/test/com/piggymetrics/accountv2/repository/AccountDBProviderImplTest.java
+++ account-service-v2/src/test/com/piggymetrics/accountv2/repository/AccountDBProviderImplTest.java
@@ -4,12 +4,12 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonObject;
-import com.piggymetrics.accountv2.domain.Account;
-import com.piggymetrics.accountv2.domain.Item;
-import com.piggymetrics.accountv2.domain.LastSeen;
+import com.piggymetrics.accountv2.domain.*;
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -18,7 +18,57 @@ public class AccountDBProviderImplTest {
     private String jsonData = "{ \"_id\" : \"demo\", \"lastSeen\" : { \"$date\" : 1542648276849 }, \"note\" : \"demo note\", \"expenses\" : [{ \"amount\" : 1300.0, \"currency\" : \"USD\", \"icon\" : \"home\", \"period\" : \"MONTH\", \"title\" : \"Rent\" }, { \"amount\" : 120.0, \"currency\" : \"USD\", \"icon\" : \"utilities\", \"period\" : \"MONTH\", \"title\" : \"Utilities\" }, { \"amount\" : 20.0, \"currency\" : \"USD\", \"icon\" : \"meal\", \"period\" : \"DAY\", \"title\" : \"Meal\" }, { \"amount\" : 240.0, \"currency\" : \"USD\", \"icon\" : \"gas\", \"period\" : \"MONTH\", \"title\" : \"Gas\" }, { \"amount\" : 3500.0, \"currency\" : \"EUR\", \"icon\" : \"island\", \"period\" : \"YEAR\", \"title\" : \"Vacation\" }, { \"amount\" : 30.0, \"currency\" : \"EUR\", \"icon\" : \"phone\", \"period\" : \"MONTH\", \"title\" : \"Phone\" }, { \"amount\" : 700.0, \"currency\" : \"USD\", \"icon\" : \"sport\", \"period\" : \"YEAR\", \"title\" : \"Gym\" }], \"incomes\" : [{ \"amount\" : 42000.0, \"currency\" : \"USD\", \"icon\" : \"wallet\", \"period\" : \"YEAR\", \"title\" : \"Salary\" }, { \"amount\" : 500.0, \"currency\" : \"USD\", \"icon\" : \"edu\", \"period\" : \"MONTH\", \"title\" : \"Scholarship\" }], \"saving\" : { \"amount\" : 5900.0, \"capitalization\" : false, \"currency\" : \"USD\", \"deposit\" : true, \"interest\" : 3.32 } }";
 
     @Test
+    public void verifyAccountGsonToPojo() {
+        Account account = jsonToPojo();
+
+        assert account.getName() instanceof String;
+        assert account.getLastSeen() instanceof LastSeen;
+        assert account.getNote() instanceof String;
+
+        for(Object expense : account.getExpenses()){
+            assert expense instanceof Item;
+        }
+
+        for(Object income : account.getIncomes()){
+            assert income instanceof Item;
+        }
+
+        assert account.getSaving() instanceof Saving;
+    }
+
+    @Test
+    public void verifyItemGsonToPojo(){
+        Account account = jsonToPojo();
+
+        for(Item expense : account.getExpenses()){
+            assert expense.getAmount() instanceof BigDecimal;
+            assert expense.getCurrency() instanceof String;
+            assert expense.getIcon() instanceof String;
+            assert expense.getPeriod() instanceof TimePeriod;
+            assert expense.getTitle() instanceof String;
+        }
+
+        for(Item income : account.getIncomes()){
+            assert income.getAmount() instanceof BigDecimal;
+            assert income.getCurrency() instanceof String;
+            assert income.getIcon() instanceof String;
+            assert income.getPeriod() instanceof TimePeriod;
+            assert income.getTitle() instanceof String;
+        }
+    }
+
+    @Test
     public void gsonToPojoCorrectLastSeenTest() {
+
+        Account account = jsonToPojo();
+
+        assert account.getLastSeen() instanceof LastSeen;
+        assert account.getLastSeen().getDate() instanceof Date;
+        assert account.getLastSeen().getDate().equals(new Date(1542648276849L));
+
+    }
+
+    public Account jsonToPojo(){
         GsonBuilder gsonBuilder = new GsonBuilder();
 
         // Adapted from:
@@ -30,8 +80,6 @@ public class AccountDBProviderImplTest {
 
         Account account = gson.fromJson(this.jsonData, Account.class);
 
-        assert account.getLastSeen() instanceof LastSeen;
-        assert account.getLastSeen().getDate() instanceof Date;
-        assert account.getLastSeen().getDate().equals(new Date(1542648276849L));
+        return account;
     }
 }
